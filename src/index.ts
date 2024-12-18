bc0af92d6278fb8ee617920575433ce232eeedd9
priyash7 <priyashshah4@gmail.com>
2024-12-18 16:19:10 +0530

    basic backend layout

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User{
+    socket : WebSocket;
+    room : string;
+}
+
+
+
+let allSockets : User[] = [];
+
+wss.on("connection", (socket) => {
+
+
+    socket.on("message",(message)=>{
+        const parsedMessage = JSON.parse(message.toString());
+        if(parsedMessage.type === "join"){
+            allSockets.push({
+                socket,
+                room : parsedMessage.payload.roomId
+            })
+        }
+
+        if(parsedMessage.type === "chat"){
+            const currentUserRoom = allSockets.find((x)=> x.socket== socket)?.room;
+
+            allSockets.forEach((user)=>{
+                if(user.room == currentUserRoom){
+                    user.socket.send(parsedMessage.payload.message);
+                }
+            })
+        }
+    })
+
+
+});
